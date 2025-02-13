9bbad00da5ec456a4affb3079acb1f046110aae0
labsfiap <labsfiap@PA705MICRO28.fiap.com.br>
2025-02-13 11:50:10 -0300

    arrumando código

--- myconnection.sql
+++ myconnection.sql
@@ -1,23 +1,14 @@
-DROP TABLE cliente cascade constraints;
-DROP TABLE genero;
-DROP TABLE tipo_cliente;
-DROP TABLE end_cliente;
-DROP TABLE bairro;
-DROP TABLE cidade;
-DROP TABLE estado;
-DROP TABLE pais;
-
 CREATE TABLE cliente (
-    id_cliente      NUMBER,
-    nome            VARCHAR(30),
-    data_nasc       DATE,
-    cpf_cnpj        NUMBER,
-    id_genero       NUMBER,
+    id_cliente  NUMBER PRIMARY KEY,
+    nome        VARCHAR2(30),
+    data_nasc   DATE,
+    cpf_cnpj    NUMBER,
+    id_genero   NUMBER,
     id_tipo_cli NUMBER
 );
 
 CREATE TABLE genero (
-    id_genero NUMBER,
+    id_genero NUMBER PRIMARY KEY,
     descricao VARCHAR(30)
 );
 
@@ -27,47 +18,49 @@ ALTER TABLE cliente
 
 CREATE TABLE tipo_cliente (
     id_tipo_cli NUMBER PRIMARY KEY,
-    descricao   VARCHAR(30)
+    descricao   VARCHAR2(30)
 );
 
 ALTER TABLE cliente
     ADD CONSTRAINT fk_tipo_cli FOREIGN KEY ( id_genero )
         REFERENCES tipo_cliente ( id_tipo_cli );
-        
+
 CREATE TABLE end_cliente (
-    id_end     NUMBER PRIMARY KEY,
-    logradouro VARCHAR2(30),
-    numero     NUMBER,
-    cep        NUMBER,
-    comlemento VARCHAR2(50),
-    referemcia VARCHAR2(30),
-    id_bairro
+    id_end      NUMBER PRIMARY KEY,
+    logradouro  VARCHAR2(30),
+    numero      NUMBER,
+    cep         NUMBER,
+    complemento VARCHAR2(50),
+    referencia  VARCHAR2(30),
+    id_bairro   NUMBER
 );
+    
+--ALTER TABLE END_CLIENTE ADD PRIMARY KEY(ID_END);
 
 CREATE TABLE bairro (
     id_bairro   NUMBER PRIMARY KEY,
-    nome_bairro VARCHAR(30),
+    nome_bairro VARCHAR2(30),
     id_estado   NUMBER
 );
 
 ALTER TABLE end_cliente
     ADD CONSTRAINT fk_bairro FOREIGN KEY ( id_bairro )
         REFERENCES bairro ( id_bairro );
-        
-CREATE TABLE estado (
-    id_estado   NUMBER PRIMARY KEY,
-    nome_estado VARCHAR2(30),
-    id_pais     NUMBER
-);
 
-ALTER TABLE bairro
-    ADD CONSTRAINT fk_estado FOREIGN KEY ( id_estado )
-        REFERENCES estado ( id_estado );
-        
 CREATE TABLE cidade (
     id_cidade   NUMBER PRIMARY KEY,
     nome_cidade VARCHAR2(30),
     id_estado   NUMBER
+);
+
+ALTER TABLE bairro
+    ADD CONSTRAINT fk_estado FOREIGN KEY ( id_cidade )
+        REFERENCES cidade ( id_cidade );
+
+CREATE TABLE estado (
+    id_estado   NUMBER PRIMARY KEY,
+    nome_estado VARCHAR2(30),
+    id_pais     NUMBER
 );
 
 ALTER TABLE cidade
@@ -82,3 +75,7 @@ CREATE TABLE pais (
 ALTER TABLE estado
     ADD CONSTRAINT fk_pais FOREIGN KEY ( id_pais )
         REFERENCES pais ( id_pais );
+
+
+
+
